b2b332b0f99fac574b74ea2512e8c1360be3159c
SheonXJ <s87462830@gmail.com>
2022-07-10 19:33:18 +0800

    feat: add api error handler & delete restaurant api

--- routes/apis/modules/admin.js
+++ routes/apis/modules/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const adminController = require('../../../controllers/apis/admin-controller')
 
+router.all('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
