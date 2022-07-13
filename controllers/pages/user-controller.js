db991c7c5a5809014c91e16d1361d6d56f42500e
SheonXJ <s87462830@gmail.com>
2022-07-13 17:10:26 +0800

    feat: add user-services & api

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs')
+const userServices = require('../../services/user-services')
 const { User, Comment, Restaurant, Favorite, Like, Followship } = require('../../models')
 const { imgurFileHandler } = require('../../helpers/file-helper')
 
@@ -7,24 +7,7 @@ const userController = {
     return res.render('signup')
   },
   signUp: (req, res, next) => {
-    // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
-    // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exist!')
-        return bcrypt.hash(req.body.password, bcrypt.genSaltSync(10))
-      })
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
-      .then(() => {
-        req.flash('success_messages', '成功註冊帳號！')
-        return res.redirect('/signin')
-      })
-      .catch(err => next(err))
+    userServices.signUp(req, (err, data) => err ? next(err) : req.flash('success_messages', '成功註冊帳號！') && res.redirect('/signin'))
   },
   signInPage: (req, res) => {
     return res.render('signin')
